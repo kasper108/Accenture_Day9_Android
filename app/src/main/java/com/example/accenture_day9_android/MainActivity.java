9a4cb612e925f2b88a8d976e777b02699eada491
kasper108 <stanislovas.kasperiunas@gmail.com>
2022-08-25 11:08:39 +0300

    Buttons added

--- app/src/main/java/com/example/accenture_day9_android/MainActivity.java
+++ app/src/main/java/com/example/accenture_day9_android/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.accenture_day9_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void oneBTN(View view){
+
+    }
+
+    public void twoBTN(View view){
+
+    }
+
+    public void threeBTN(View view){
+
+    }
+
+    public void fourBTN(View view){
+
+    }
+
+    public void fiveBTN(View view){
+
+    }
+
+    public void sixBTN(View view){
+
+    }
+
+    public void sevenBTN(View view){
+
+    }
+
+    public void eightBTN(View view){
+
+    }
+
+
+    public void nineBTN(View view){
+
+    }
+
+    public void zeroBTN(View view){
+
+    }
+
 }
